731ef7e8d2bbbcbdfad0a72db7f4c8b8403270e3
alpbourne <alpbourne@gmail.com>
2017-09-29 16:37:45 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,11 +2,16 @@ class Author
 
   attr_accessor :name, :posts, :post
 
+  @@post_count = 0
+
   def initialize(name)
     @name = name
     @posts = []
   end
 
   def add_post(post)
-
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
 end
